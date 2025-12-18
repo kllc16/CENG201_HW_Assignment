9ee54ba6f7e42d0fddd0318c03288b018a011b2f
yusufkeremkelleci <yusufkeremkelleci@gmail.com>
2025-12-18 07:48:13 +0300

    ı create the first classes

---
+++ src/Patient.java
@@ -0,0 +1,14 @@
+public class Patient {
+    //implemented wanted attributes
+    int id;
+    String name;
+    int severity;
+    int age;
+    //then created its constructor
+    public Patient(int id, String name, int severity, int age) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+        this.age = age;
+    }
+}
