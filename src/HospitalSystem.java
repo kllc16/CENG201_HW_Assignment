ee436bcc28d04adb3eec44fd202748415a3293b6
yusufkeremkelleci <yusufkeremkelleci@gmail.com>
2025-12-24 15:16:13 +0300

    Generated needed constructer

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+
 public class HospitalSystem {
+    private PatientList patientList;
+    private TreatmentQueue treatmentQueue;
+    private DischargeStack dischargeStack;
+    private HashMap<Integer, Patient> patientHashMap;
+
+    public HospitalSystem(PatientList patientList, TreatmentQueue treatmentQueue, DischargeStack dischargeStack, HashMap<Integer, Patient> patientHashMap) {
+        this.patientList = new PatientList();
+        this.treatmentQueue = new TreatmentQueue();
+        this.dischargeStack = new DischargeStack();
+        this.patientHashMap = new HashMap<>();
+    }
 }
